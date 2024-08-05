8eb53240b65ed5c97ca66976ad38063b2f9e8bc9
Terry Easley <easleter000@warren.k12.in.us>
2024-08-05 13:31:07 -0400

    U1L1.3

---
+++ src/operators.java
@@ -0,0 +1,12 @@
+public class operators {
+    public static void main(String[] args) {
+        byte byteValue = 12;
+        byte shortValue = 22;
+        int intValue = 2;
+        long longTotal = 80000L - 5L * (byteValue + shortValue + intValue);
+        System.out.println(longTotal);
+
+
+
+    }
+}
